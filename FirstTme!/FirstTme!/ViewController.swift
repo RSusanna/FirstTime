029b0d0802955337f0c288688f5691d08af4140c
Susanna <susanna1235@gmail.com>
2023-05-29 18:44:22 +0300

    Made secret button work

--- FirstTme!/FirstTme!/ViewController.swift
+++ FirstTme!/FirstTme!/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var toggleBotton: UIButton!
+    @IBOutlet var helloWorldLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        helloWorldLabel.isHidden = true // лейбл хело ворлд скрыт
+        toggleBotton.layer.cornerRadius = 5
+}
+
+    @IBAction func makeButtonAct(_ sender: Any) {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            toggleBotton.setTitle("Hide Text", for: .normal)
+        } else {
+        helloWorldLabel.isHidden = true
+        toggleBotton.setTitle("Secret button", for: .normal)
+        }
     }
-
-
 }
 
